b27caa45948c15634fdd70818c8fa1ca3721779a
0sunset0 <chi6465618@naver.com>
2025-06-25 23:33:36 +0900

    initial commit

---
+++ yakssok/src/main/java/server/yakssok/YakssokApplication.java
@@ -0,0 +1,13 @@
+package server.yakssok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YakssokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YakssokApplication.class, args);
+	}
+
+}
---
+++ yakssok/src/test/java/server/yakssok/YakssokApplicationTests.java
@@ -0,0 +1,13 @@
+package server.yakssok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YakssokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
