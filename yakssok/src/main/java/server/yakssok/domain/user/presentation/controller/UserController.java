2810aae6452a22e7c9340a26f32697de2c111a7a
0sunset0 <chi6465618@naver.com>
2025-07-13 00:52:48 +0900

    feat: [YSS-35] 초대 코드로 유저 정보 조회

--- yakssok/src/main/java/server/yakssok/domain/user/presentation/controller/UserController.java
+++ yakssok/src/main/java/server/yakssok/domain/user/presentation/controller/UserController.java
@@ -5,15 +5,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import server.yakssok.domain.user.application.service.UserService;
-import server.yakssok.domain.user.presentation.dto.request.UpdateUserInfoRequest;
 import server.yakssok.domain.user.presentation.dto.response.FindUserInfoResponse;
+import server.yakssok.domain.user.presentation.dto.request.UpdateUserInfoRequest;
+import server.yakssok.domain.user.presentation.dto.response.FindMyInfoResponse;
 import server.yakssok.domain.user.presentation.dto.response.FindUserInviteCodeResponse;
 import server.yakssok.global.common.reponse.ApiResponse;
 import server.yakssok.global.common.security.YakssokUserDetails;
@@ -27,14 +30,14 @@ import server.yakssok.global.exception.ErrorCode;
 public class UserController {
 	private final UserService userService;
 
-	@Operation(summary = "유저 정보 조회(프로필, 닉네임)")
+	@Operation(summary = "내 정보 조회")
 	@ApiErrorResponse(ErrorCode.NOT_FOUND_USER)
 	@GetMapping("/me")
-	public ApiResponse<FindUserInfoResponse> findUserInfo(
+	public ApiResponse<FindMyInfoResponse> findMyInfo(
 		@AuthenticationPrincipal YakssokUserDetails userDetails
 	) {
 		Long userId = userDetails.getUserId();
-		return ApiResponse.success(userService.findUserInfo(userId));
+		return ApiResponse.success(userService.findMyInfo(userId));
 	}
 
 	@Operation(summary = "유저 정보 수정(프로필, 닉네임)")
@@ -57,5 +60,16 @@ public class UserController {
 	) {
 		Long userId = userDetails.getUserId();
 		return ApiResponse.success(userService.findUserInviteCode(userId));
+	}
+
+	@Operation(summary = "초대 코드로 유저 정보 조회")
+	@ApiErrorResponse(ErrorCode.NOT_FOUND_USER)
+	@ApiErrorResponse(ErrorCode.INTERNAL_SERVER_ERROR)
+	@GetMapping
+	public ApiResponse<FindUserInfoResponse> findUserInfoByInviteCode(
+		@Parameter(description = "초대 코드", required = true, example = "JoWRYS3Fz")
+		@RequestParam String inviteCode
+	) {
+		return ApiResponse.success(userService.findUserInfoByInviteCode(inviteCode));
 	}
 }
--- yakssok/src/main/java/server/yakssok/domain/user/presentation/dto/response/FindMyInfoResponse.java
+++ yakssok/src/main/java/server/yakssok/domain/user/presentation/dto/response/FindMyInfoResponse.java
@@ -2,7 +2,7 @@ package server.yakssok.domain.user.presentation.dto.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
-public record FindUserInfoResponse(
+public record FindMyInfoResponse(
 	@Schema(description = "닉네임", example = "노을")
 	String nickname,
 	@Schema(description = "프로필 이미지 URL", example = "https://example.com/profile.jpg")
