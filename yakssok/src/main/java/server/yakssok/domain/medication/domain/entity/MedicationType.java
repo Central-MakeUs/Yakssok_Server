5169afd14902a6522997100beab237ef8ddaeb4f
0sunset0 <chi6465618@naver.com>
2025-07-15 14:17:49 +0900

    refactor: [YSS-40] 불필요한 필드 제거

--- yakssok/src/main/java/server/yakssok/domain/medication/domain/entity/MedicationType.java
+++ yakssok/src/main/java/server/yakssok/domain/medication/domain/entity/MedicationType.java
@@ -6,20 +6,14 @@ import lombok.Getter;
 import server.yakssok.domain.medication.application.exception.MedicationException;
 @Getter
 public enum MedicationType {
-	CHRONIC("만성 질환 관리"),
-	MENTAL("정신 건강 관리"),
-	SUPPLEMENT("건강기능식품/영양보충"),
-	BEAUTY("미용 관련 관리"),
-	HIGHRISK("고위험군 복약"),
-	DIET("다이어트/대사 관련"),
-	TEMPORARY("통증/감기 등 일시적 치료"),
-	OTHER("기타 설정");
-
-	private final String label;
-
-	MedicationType(String label) {
-		this.label = label;
-	}
+	CHRONIC,
+	MENTAL,
+	SUPPLEMENT,
+	BEAUTY,
+	HIGHRISK,
+	DIET,
+	TEMPORARY,
+	OTHER;
 
 	public static MedicationType from(String name) {
 		try {
