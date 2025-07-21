255492187470e5d93771a92c1b1053afcb5c37ff
0sunset0 <chi6465618@naver.com>
2025-07-21 20:31:14 +0900

    feat: [YSS-53] 더미 계정 생성

--- yakssok/src/main/java/server/yakssok/domain/auth/presentation/dto/request/JoinRequest.java
+++ yakssok/src/main/java/server/yakssok/domain/auth/presentation/dto/request/JoinRequest.java
@@ -25,7 +25,6 @@ public record JoinRequest(
 	Boolean pushAgreement,
 
 	@Schema(description = "FCM 토큰", example = "1234567890abcdef")
-	@NotNull
 	String fcmToken
 ) {
 	public User toUser(String providerId, String profileImageUrl) {
