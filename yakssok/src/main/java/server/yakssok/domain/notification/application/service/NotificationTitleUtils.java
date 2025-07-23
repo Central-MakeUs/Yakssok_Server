70aa173c4b348bb5a829716312c7bf8f078d3cb3
0sunset0 <chi6465618@naver.com>
2025-07-23 21:02:13 +0900

    feat: [YSS-48] 알림 제목 유틸 클래스

--- yakssok/src/main/java/server/yakssok/domain/notification/application/service/NotificationTitleUtils.java
+++ yakssok/src/main/java/server/yakssok/domain/notification/application/service/NotificationTitleUtils.java
@@ -3,11 +3,35 @@ package server.yakssok.domain.notification.application.service;
 import server.yakssok.domain.feeback.domain.entity.FeedbackType;
 
 public class NotificationTitleUtils {
+	private static final String FEEDBACK_PRAISE_FORMAT = "%s님이 보낸 칭찬!";
+	private static final String FEEDBACK_NAG_FORMAT = "%s님이 보낸 잔소리!";
+	private static final String FEEDBACK_DEFAULT_FORMAT = "%s님이 보낸 피드백!";
+
+	private static final String MEDICATION_GUIDE_FORMAT = "%s 챙길 시간이에요!";
+	private static final String MEDICATION_REMINDER_FORMAT = "%s님~ %s 안 먹었어요!";
+	private static final String FRIEND_NOT_TAKEN_FORMAT = "%s님의 %s가 약을 안먹었어요";
+
+
 	public static String createFeedbackTitle(FeedbackType type, String senderName) {
 		switch(type) {
-			case PRAISE: return senderName + "님이 보낸 칭찬!";
-			case NAG:    return senderName + "님이 보낸 잔소리!";
-			default:     return senderName + "님이 보낸 피드백!";
+			case PRAISE:
+				return String.format(FEEDBACK_PRAISE_FORMAT, senderName);
+			case NAG:
+				return String.format(FEEDBACK_NAG_FORMAT, senderName);
+			default:
+				return String.format(FEEDBACK_DEFAULT_FORMAT, senderName);
 		}
+	}
+
+	public static String createMedicationGuideTitle(String medicineName) {
+		return String.format(MEDICATION_GUIDE_FORMAT, medicineName);
+	}
+
+	public static String createMedicationReminderTitle(String userName, String medicineName) {
+		return String.format(MEDICATION_REMINDER_FORMAT, userName, medicineName);
+	}
+
+	public static String createFriendNotTakenAlarmTitle(String userName, String mateRole) {
+		return String.format(FRIEND_NOT_TAKEN_FORMAT, userName, mateRole);
 	}
 }
