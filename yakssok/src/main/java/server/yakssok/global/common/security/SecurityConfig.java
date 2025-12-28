a601d8cacb9a057194a0c0790b9e66cdf719cd41
0sunset0 <chi6465618@naver.com>
2025-12-28 21:05:48 +0900

    build: 애플 server to server 요청 허용

--- yakssok/src/main/java/server/yakssok/global/common/security/SecurityConfig.java
+++ yakssok/src/main/java/server/yakssok/global/common/security/SecurityConfig.java
@@ -28,7 +28,8 @@ public class SecurityConfig {
 		"/api/auth/login/**",
 		"/api/auth/reissue/**",
 		"/api/auth/kakao/callback",
-		"/actuator/**"
+		"/actuator/**",
+		"/apple/notifications"
 	};
 
 	@Bean
